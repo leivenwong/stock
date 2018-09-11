473208ce95eb2931804927923c4a953ed2eb3923
leivenwong <13852904@qq.com>
2018-09-11 11:02:25 +0800

    add fake_spider_run

---
+++ new_stock/fake_spider_run.py
@@ -0,0 +1,31 @@
+import os
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/tushare/hs300.py")
+print("hs300 has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/tushare/kData.py")
+print("dData has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/tushare/stockList.py")
+print("stockList has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/cwsj.py")
+print("cwsj has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/gpfh.py")
+print("gpfh has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/m012.py")
+print("m012 has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/spider.py")
+print("spider has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/yjjg.py")
+print("yjjg has run.")
+
+os.system("python D:/stock_python/stock/new_stock/fake_spider/zgb2.py")
+print("zgb2 has run.")
+
+os.system("python D:/stock_python/stock/new_stock/adjust/cwsj_manager.py")
+print("cwsj_manager has run.")
